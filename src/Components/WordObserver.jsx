00b28f836346d0ddbf0a25a5201fd11188f66b22
Charles Huang <chuang@labattfood.com>
2019-05-23 16:26:30 -0400

    Added some observable logic
    Observable logic

--- src/Components/WordObserver.jsx
+++ src/Components/WordObserver.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import Observable from "../Scripts/Observable";
-//TODO: Test should component update in here.
+//TODO: Test should component update in here. create higher order componnet to show render count
 //Implement a component that takes a word input and displays all combinations, permutations, word length, and word reversed.
 export default class WordObserver extends React.PureComponent {
   constructor(props) {
     super(props);
     this.observable = new Observable();
+    this.observable.subscribe(this.getPermutations);
+    this.observable.subscribe(this.getCombinations);
     this.state = {
       inputText: ""
     };
   }
+
+  initializeObserver = context => {};
+
+  getPermutations = data => {
+    document.querySelector("#permutations").textContent = "Permutations!";
+  };
+
+  getCombinations = data => {
+    document.querySelector("#combinations").textContent = "Combinations";
+  };
   render() {
     return (
-      <div id="wordObserverContainer">
-        <input inputText="hello" />
+      <div>
+        <div id="wordObserverContainer">
+          <input type="text" name="wordObserver" value={this.state.inputText} />
+        </div>
+        <div id="permuations" />
+        <div id="combinations" />
       </div>
     );
   }
